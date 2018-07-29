2b22960f6e3eb681cca73e430b439ca1c540a24b
Kwirke <nebrios2@gmail.com>
2018-07-29 23:32:46 +0200

    Implement SearchFormView

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,18 +1,31 @@
 import { connect } from 'react-redux';
 import { notesSelector, categoriesSelector } from '../../redux/appState';
 import { selectedCategorySelector } from './SearchFormState';
-import { SearchFormView, IProps } from './SearchFormView';
+import { SearchFormView, IProps, IActions } from './SearchFormView';
 import * as R from 'ramda';
+import { DropdownItemProps } from '../../../node_modules/semantic-ui-react';
+import { selectCategory } from './SearchFormActions';
+import { Maybe } from 'tsmonad';
 
+const nameToDropdownItem = (name: string): DropdownItemProps => ({
+    text: name,
+    value: name,
+});
+
+const selectCategories = state => (
+    R.values(categoriesSelector(state))
+        .map(R.prop('title'))
+        .map(nameToDropdownItem)
+)
 
 const mapStateToProps = (state): IProps => ({
     // notes: notesSelector(state),
-    categories: R.values(categoriesSelector(state)).map(R.prop('title')),
+    categories: selectCategories(state),
     selectedCategory: selectedCategorySelector(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-
+const mapDispatchToProps = (dispatch): IActions => ({
+    onCategorySelect: (category) => dispatch(selectCategory(Maybe.maybe(category))),
 })
 
 export const SearchForm = connect(mapStateToProps, mapDispatchToProps)(SearchFormView);
--- src/components/SearchForm/SearchFormView.tsx
+++ src/components/SearchForm/SearchFormView.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import { Maybe } from 'tsmonad';
+import { Dropdown, DropdownItemProps } from 'semantic-ui-react';
+import * as R from 'ramda';
 
 export interface IProps {
-    categories: string[];
+    categories: DropdownItemProps[];
     selectedCategory: Maybe<string>;
 }
 
-export const SearchFormView = ({categories, selectedCategory}) => (
-    <div></div>
+export interface IActions {
+    onCategorySelect: (category: string) => void;
+}
+
+export const SearchFormView = ({categories, selectedCategory, onCategorySelect}) => (
+    <Dropdown
+        search
+        deburr
+        selection
+        options={categories}
+        text={selectedCategory.valueOr('Select a category')}
+        value={selectedCategory.valueOr(false)}
+        onChange={(e, {value}) => onCategorySelect(value)}
+    />
 );
