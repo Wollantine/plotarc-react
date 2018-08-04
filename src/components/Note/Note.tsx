f2457e02f035e0442ba30fbe66a58d5944c70a5f
Kwirke <nebrios2@gmail.com>
2018-08-04 21:51:42 +0200

    Create Note component

---
+++ src/components/Note/Note.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { createComponent } from 'react-fela';
+import { Item } from 'semantic-ui-react';
+import { Note as NoteType } from 'model/Types';
+
+
+const NoteCard = createComponent(() => ({
+    backgroundColor: 'white !important',
+    borderRadius: '5px !important',
+    border: '1px solid #eee !important',
+    boxShadow: '2px 2px 2px -1px #ccc !important',
+    ':hover': {
+        boxShadow: '2px 2px 4px 0px #bbb !important'
+    }
+}), Item as any)
+
+
+export const Note: React.StatelessComponent<NoteType> = ({title}) => (
+    <NoteCard>
+        <Item.Content>
+            <Item.Header>{title}</Item.Header>
+            <Item.Description>Blabla bla bla bla bla bla bla bllabla bla bla bla bla bla bla blaa</Item.Description>
+        </Item.Content> 
+    </NoteCard>
+)
--- src/components/NoteList/NoteListView.tsx
+++ src/components/NoteList/NoteListView.tsx
@@ -1,31 +1,15 @@
 import * as React from 'react';
 import { Item } from 'semantic-ui-react';
-import { Note } from 'model/Types';
+import { Note as NoteType } from 'model/Types';
 import { createComponent } from 'react-fela';
+import { Note } from '../Note/Note';
 
 export interface IProps {
-    notes: Note[];
+    notes: NoteType[];
 }
-
-const NoteCard = createComponent(() => ({
-    backgroundColor: 'white !important',
-    borderRadius: '5px !important',
-    border: '1px solid #eee !important',
-    boxShadow: '2px 2px 2px -1px #ccc !important',
-    ':hover': {
-        boxShadow: '2px 2px 4px 0px #bbb !important'
-    }
-}), Item as any)
 
 export const NoteListView: React.StatelessComponent<IProps> = ({notes}) => (
     <Item.Group>
-        {notes.map(({title}) => (
-            <NoteCard key={title}>
-                <Item.Content>
-                    <Item.Header>{title}</Item.Header>
-                    <Item.Description>Blabla bla bla bla bla bla bla bllabla bla bla bla bla bla bla blaa</Item.Description>
-                </Item.Content> 
-            </NoteCard>
-        ))}
+        {notes.map(note => <Note key={note.title} {...note}/>)}
     </Item.Group>
-);
+)
