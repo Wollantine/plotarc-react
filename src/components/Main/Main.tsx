2cbdbb65d8839cdfc0864b037b503f8d6acb89d5
Kwirke <nebrios2@gmail.com>
2018-08-05 12:37:04 +0200

    Make sidebar fixed

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,23 +1,10 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { SearchForm } from '../SearchForm/SearchForm';
-import { NoteList } from '../NoteList/NoteList';
 import { createComponent } from 'react-fela';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Sticky } from 'semantic-ui-react';
 import { ListDisplay } from '../ListDisplay/ListDisplay';
 
-
-const CustomGrid = createComponent(() => ({
-    display: 'grid',
-    gridTemplateColumns: '1fr 4fr',
-    gridColumnGap: '30px',
-}))
-
-const Box = createComponent(() => ({
-    backgroundColor: 'white',
-    // border: '2px solid #',
-    boxShadow: 'rgb(204, 204, 204) 0px 1px 2px',
-}))
 
 const Sidebar = createComponent(() => ({
     padding: '50px 50px 0 50px',
@@ -31,7 +18,9 @@ const Container = createComponent(() => ({
 const MainContainer = () => (
     <Grid stackable>
         <Grid.Column width={5}>
-            <Sidebar><SearchForm/></Sidebar>
+            <Sticky>
+                <Sidebar><SearchForm/></Sidebar>
+            </Sticky>
         </Grid.Column>
         <Grid.Column width={11}>
             <Container><ListDisplay/></Container>
